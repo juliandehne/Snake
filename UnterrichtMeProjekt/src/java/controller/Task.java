ef2673d8ebd2f950553e2089a5f17345a3fe08c9
root <root@clemans.einstein>
2013-11-04 12:47:22 +0100

    improved performance of pic loading

--- UnterrichtMeProjekt/src/java/controller/Task.java
+++ UnterrichtMeProjekt/src/java/controller/Task.java
@@ -5,6 +5,7 @@
 package controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpSessionBindingEvent;
@@ -27,10 +28,16 @@ public class Task extends Thread implements HttpSessionBindingListener {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void run() {
+    public void run()  {
         while (true) {
             CreatePicture instance = new CreatePicture();
-            instance.paintPicture(deployStream);
+            File tmp = new File(deployStream.getParent()+"tmp.png");
+            instance.paintPicture(tmp);
+            try {
+                instance.copyPicture(tmp, deployStream);
+            } catch (IOException ex) {
+                Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
+            }
             try {
                 this.sleep(800);
             } catch (InterruptedException ex) {
