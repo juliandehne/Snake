6c34ceec7b8ecc844d173c483c0a62a2508a603e
root <dehne@uni-potsdam.de>
2013-11-18 09:05:11 +0100

    adding example for public private ...

---
+++ UnterrichtMeProjekt/src/java/Snake.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jdehne
+ */
+public class Snake {
+    public int position_x;
+    public int position_y;
+}
