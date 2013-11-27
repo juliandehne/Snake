d8d2adf8ad28e08d359c93deea4aa854bbc5b37e
uzuzjmd <julian.dehne@gmail.com>
2013-11-27 16:27:58 +0100

    commiting renes ergebnisse to master

---
+++ UnterrichtMeProjekt/test/logik/SnakeTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logik;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author weichert
+ */
+public class SnakeTest {
+    
+    public SnakeTest() {
+    }
+    
+    
+
+
+}
