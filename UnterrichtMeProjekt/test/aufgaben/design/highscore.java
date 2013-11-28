1366534fc253df9a0cad5bb64f20d4b1284621f8
RiseDevastation <forthmann@gmx.net>
2013-11-28 21:55:40 +0100

    test-commit

---
+++ UnterrichtMeProjekt/test/aufgaben/design/highscore.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aufgaben.design;
+
+/**
+ *
+ * @author IceMan
+ */
+public class highscore {
+    
+}
