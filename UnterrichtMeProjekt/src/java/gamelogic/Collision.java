164b0ec17c28ee1b28780601e4c465f025ef3816
IRawXI <jaguar122@freenet.de>
2013-12-09 09:45:32 +0100

    Entkopplung Collision

--- UnterrichtMeProjekt/src/java/gamelogic/Collision.java
+++ UnterrichtMeProjekt/src/java/gamelogic/Collision.java
@@ -17,4 +17,65 @@ public class Collision {
      
     
     }
+
+    Collision(PlayingGround aThis) {
+        this.playingground = aThis;
+    }
+    
+    
+     /**
+     * Hauptmehtode zur Logikbehandlung Design: um die Schlange zu updaten,
+     * diese direkt verändern, folgende Methode merkt dies dann
+     * @return returns whether the game still running (->Game Over)
+     */
+    public boolean update() {
+
+        
+        for (int x = 0; x < playingground.length; x++) {
+            for (int y = 0; y < playingground.playingGround[x].length; y++) {
+                if (x == playingground.snake.getPos().getX() && y == snake.getPos().getY() && 
+                    playingGround[x][y] == PositionType.BORDER) {
+                    //GAME ZU ENDE, false wird zurückgegeben
+                    return false;
+                }
+                //Erweiterung durch René Weichert
+                else{
+                    if (x == snake.getPos().getX() && y == snake.getPos().getY() && 
+                        playingGround[x][y] == PositionType.SNAKETAIL) {
+                        return false;
+                    }
+                    else{
+                        if(x == snake.getPos().getX() && y == snake.getPos().getY() && 
+                            playingGround[x][y] == PositionType.ITEMNORMAL){
+                            
+                        }
+                        else{
+                            if (x == snake.getPos().getX() && y == snake.getPos().getY() && 
+                                playingGround[x][y] == PositionType.ITEMSHORT) {
+                                
+                            }
+                            else{
+                                if (x == snake.getPos().getX() && y == snake.getPos().getY() && 
+                                    playingGround[x][y] == PositionType.ITEMFAST) {
+                                    
+                                }
+                                else{
+                                    return true;
+                                }
+                            }
+                        }
+                            
+                                
+                    }
+                    
+                }
+                    
+                playingGround[x][y] = populateArrayAt(x, y, 
+                        this.playingGround.length -1,
+                        this.playingGround[x].length-1);
+                //weitere logik behandlung (items usw...)...
+            }
+        } 
+        return false;
+    }
 }
--- UnterrichtMeProjekt/src/java/gamelogic/PlayingGround.java
+++ UnterrichtMeProjekt/src/java/gamelogic/PlayingGround.java
@@ -16,68 +16,16 @@ public class PlayingGround {
     private final Snake snake;
     private final PositionType[][] playingGround;
     private ArrayList items = new ArrayList();
+    private Collision collision = null;
 
     public PlayingGround(int sizeX, int sizeY, Snake snake) {
         this.playingGround = new PositionType[sizeX][sizeY];
         this.snake = snake;
-        this.update();
+        this.collision = new Collision(this);
+        collision.update();
     }
 
-    /**
-     * Hauptmehtode zur Logikbehandlung Design: um die Schlange zu updaten,
-     * diese direkt verändern, folgende Methode merkt dies dann
-     * @return returns whether the game still running (->Game Over)
-     */
-    public boolean update() {
-
-        
-        for (int x = 0; x < this.playingGround.length; x++) {
-            for (int y = 0; y < this.playingGround[x].length; y++) {
-                if (x == snake.getPos().getX() && y == snake.getPos().getY() && 
-                    playingGround[x][y] == PositionType.BORDER) {
-                    //GAME ZU ENDE, false wird zurückgegeben
-                    return false;
-                }
-                //Erweiterung durch René Weichert
-                else{
-                    if (x == snake.getPos().getX() && y == snake.getPos().getY() && 
-                        playingGround[x][y] == PositionType.SNAKETAIL) {
-                        return false;
-                    }
-                    else{
-                        if(x == snake.getPos().getX() && y == snake.getPos().getY() && 
-                            playingGround[x][y] == PositionType.ITEMNORMAL){
-                            
-                        }
-                        else{
-                            if (x == snake.getPos().getX() && y == snake.getPos().getY() && 
-                                playingGround[x][y] == PositionType.ITEMSHORT) {
-                                
-                            }
-                            else{
-                                if (x == snake.getPos().getX() && y == snake.getPos().getY() && 
-                                    playingGround[x][y] == PositionType.ITEMFAST) {
-                                    
-                                }
-                                else{
-                                    return true;
-                                }
-                            }
-                        }
-                            
-                                
-                    }
-                    
-                }
-                    
-                playingGround[x][y] = populateArrayAt(x, y, 
-                        this.playingGround.length -1,
-                        this.playingGround[x].length-1);
-                //weitere logik behandlung (items usw...)...
-            }
-        } 
-        return false;
-    }
+   
     
     public boolean bordercheck(){
         return true;
