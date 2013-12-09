7860ef269e28c17f9c680d6e49e4f8ebc0bd3ed2
uzuzjmd <julian.dehne@gmail.com>
2013-12-09 09:11:20 +0100

    Änderungen aus der Stunde

--- UnterrichtMeProjekt/src/java/database/Datenbankzugriffe.java
+++ UnterrichtMeProjekt/src/java/database/Datenbankzugriffe.java
@@ -1,5 +1,9 @@
 package database;
 
+import example.Point;
+import gamelogic.Item;
+import gamelogic.PlayingGround;
+import gamelogic.Spieler;
 import java.util.List;
 
 /**
@@ -12,7 +16,7 @@ public class Datenbankzugriffe {
      * Diese Methode mit Code füllen...
      */
     public void erstelleHighScore(List<IdNamePair> idNamePairs) {
-        throw new UnsupportedOperationException("Methode noch nicht implemetiert."); //To change body of generated methods, choose Tools | Templates.
+        throw new Error("Methode noch nicht implemetiert."); //To change body of generated methods, choose Tools | Templates.
     }
     
     /**
@@ -20,9 +24,39 @@ public class Datenbankzugriffe {
      * @return 
      */
     public List<IdNamePair> holeHighscore() {        
+        throw new Error("Methode noch nicht implemetiert."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public int getId() {
+    /// datenbankgruppe organisiert die id dies testspielers
+        throw new Error("Methode noch nicht implemetiert."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public List<Point> getPosition(int id) { 
+    return null;
+    }
+    
+    public void move(Integer id, String richtung){
+        
+    }
+    
+    public String getRichtung(int id) {
         throw new UnsupportedOperationException("Methode noch nicht implemetiert."); //To change body of generated methods, choose Tools | Templates.
     }
-
     
-
+    public PlayingGround getPlayingGround(){
+        throw new UnsupportedOperationException("Methode noch nicht implemetiert."); 
+    }
+    
+    public List<Item> getItems(){
+        throw new UnsupportedOperationException("Methode noch nicht implemetiert."); 
+    }
+    
+    public void spielerAnlegen(Spieler spieler) { 
+        // bitte implementiere
+    }
+    
+    public Spieler getSpieler(Integer id){
+         throw new UnsupportedOperationException("Methode noch nicht implemetiert."); 
+    }
 }
