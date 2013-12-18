779da78d512e244906ed942c979d65a3ba318e39
root <root@clemans.einstein>
2013-12-18 09:05:04 +0100

    dbrnuh

---
+++ UnterrichtMeProjekt/src/java/RNUH/Thread1.java
@@ -0,0 +1,24 @@
+package RNUH;
+
+/**
+ *
+ * @author Julian Dehne
+ */
+public class Thread1 extends Thread {
+    public Boolean stop;
+
+    public Thread1(Boolean stop) {
+        this.stop = stop;        
+    }
+    
+    
+
+    @Override
+    public void run() {
+        while (!stop) {
+            System.err.println("Weihnachten");
+        }
+        interrupt();
+    }
+    
+}
