4cb9a93e2d14a6619d41a0b7c9ca40604cf9a899
uzuzjmd <julian.dehne@gmail.com>
2013-12-21 13:03:07 +0100

    added log4j to project and changed some path

--- UnterrichtMeProjekt/src/java/controller/Task.java
+++ UnterrichtMeProjekt/src/java/controller/Task.java
@@ -5,22 +5,26 @@
 package controller;
 
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionBindingListener;
 import picture.CreatePicture;
 import gamelogic.*;
 import itemlogic.ItemController;
+import java.io.IOException;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
 
 /**
  *
  * @author Julian
  */
-public class Task extends Thread implements HttpSessionBindingListener {
-   
+public class Task extends Thread implements HttpSessionBindingListener {    
     //Vielleicht ein bisschen gehackt --> ja leicht, aber das ist ok
     public static Facing facing = Facing.UP;
+    private static boolean hasInitializedLogger = false;
 
     private final File deployStream;
 
@@ -31,19 +35,22 @@ public class Task extends Thread implements HttpSessionBindingListener {
     Task() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     static synchronized void setFacing(Facing facing) {
-       Task.facing = facing;
+        Task.facing = facing;
     }
 
     public void run() {
-        Snake snake = new Snake(new Position(25,25),5, Facing.LEFT);
-        PlayingGround playingGround = new PlayingGround(100,100,snake);
+        Logger logger = initLogger();
+        logger.info("starte SnakeThread");        
+
+        Snake snake = new Snake(new Position(25, 25), 5, Facing.LEFT);
+        PlayingGround playingGround = new PlayingGround(100, 100, snake);
         while (true) {
             CreatePicture instance = new CreatePicture();
-            instance.paintPicture(deployStream,playingGround.getPlayingGround()); //Size von dem Feld ist unabhängig von der größe des Pictures
+            instance.paintPicture(deployStream, playingGround.getPlayingGround()); //Size von dem Feld ist unabhängig von der größe des Pictures
             //instance.paintPicture(deployStream);
-                       
+
             // das hier muss in die paintPictureMethode und dort als Datenbankzugriff implementiert werden
             //snake.setFacing(Task.facing);                        
 //            snake.move(false);
@@ -51,14 +58,13 @@ public class Task extends Thread implements HttpSessionBindingListener {
 //            if (!gameRunning) {
 //                //return;
 //            }            
-
             ItemController itemController = new ItemController();
             itemController.spawnItems();
-            
+
             try {
                 this.sleep(800);
             } catch (InterruptedException ex) {
-                Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(Task.class.getName()).log(Level.ERROR, null, ex);
             }
             if (isInterrupted()) {
                 return;
@@ -73,4 +79,31 @@ public class Task extends Thread implements HttpSessionBindingListener {
     public void valueUnbound(HttpSessionBindingEvent event) {
         interrupt(); // Will signal interrupt when session expires.
     }
+
+    /**
+     * Diese Methode initialisiert einen Logger, den man verwenden kann,
+     * um den aktuellen Zustand des Programmes zu evaluieren
+     * 
+     * @return 
+     */
+    public static Logger initLogger()  {
+        Logger logger = Logger.getRootLogger();
+        // Initialize logging once
+        if (!hasInitializedLogger) {
+            /* it is enough to set this once, because it impacts each RootLogger */
+            /* TODO: set to error/warn for competition */
+            logger.setLevel(Level.INFO);
+            SimpleLayout layout = new SimpleLayout();
+            ConsoleAppender consoleAppend = new ConsoleAppender(layout);
+            logger.addAppender(consoleAppend);
+            try {
+                logger.addAppender(new FileAppender(layout, "Snakerrors.log"));
+            } catch (IOException ex) {
+                java.util.logging.Logger.getLogger(Task.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
+            hasInitializedLogger = true;
+        }
+        return logger;
+    }
+
 }
